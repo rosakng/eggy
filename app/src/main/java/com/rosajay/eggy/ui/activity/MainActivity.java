359e88d3c30e71407a6ae980c169454d87656664
J W <jay.weng7@gmail.com>
2019-05-11 19:47:01 -0400

    remove title bar

--- app/src/main/java/com/rosajay/eggy/ui/activity/MainActivity.java
+++ app/src/main/java/com/rosajay/eggy/ui/activity/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.rosajay.eggy.R;
 import com.rosajay.eggy.ui.fragment.HomeScreen;
@@ -12,7 +14,12 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState); requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
+
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        
         setContentView(R.layout.home_screen);
 
     }
