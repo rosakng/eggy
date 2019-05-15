1fc14d735b27883683c90481d48cd50aef65e333
J W <jay.weng7@gmail.com>
2019-05-14 23:55:27 -0400

    add/remove favourite recipes to db

--- app/src/main/java/com/rosajay/eggy/ui/adapter/RecipeSearchAdapter.java
+++ app/src/main/java/com/rosajay/eggy/ui/adapter/RecipeSearchAdapter.java
@@ -5,6 +5,7 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
@@ -14,12 +15,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.rosajay.eggy.R;
 import com.squareup.picasso.Picasso;
 
@@ -27,6 +32,9 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
 public class RecipeSearchAdapter extends RecyclerView.Adapter<RecipeSearchAdapter.ViewHolder> {
     private JSONObject mRecipes;
@@ -34,6 +42,7 @@ public class RecipeSearchAdapter extends RecyclerView.Adapter<RecipeSearchAdapte
     private static Activity mActivity;
     private static FragmentManager fragm;
     private static String URL = "", titleString = "";
+    private Set<String> favouriteRecipes;
     public RecipeSearchAdapter(JSONObject allData) {
         this.mRecipes = allData;
     }
@@ -77,17 +86,43 @@ public class RecipeSearchAdapter extends RecyclerView.Adapter<RecipeSearchAdapte
             titleString = mRecipes.getJSONArray("meals").getJSONObject(position).get("strMeal").toString();
             title.setText(titleString);
 
+            final DatabaseReference db;
+            db = FirebaseDatabase.getInstance().getReference();
+            final DatabaseReference favouriteRecipesReference = db.child("recipes");
+            ValueEventListener v = new ValueEventListener() {
+                Set<String> favouriteRecipesFromDb = new HashSet<>();
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    if(dataSnapshot.getValue() != null) {
+                        favouriteRecipesFromDb = ((HashMap<String, String>) dataSnapshot.getValue()).keySet();
+                    }
+
+                    favouriteRecipes = favouriteRecipesFromDb;
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            };
+
+            favouriteRecipesReference.addValueEventListener(v);
+
             seeMore.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
                     try{
+                        String recipeId = mRecipes.getJSONArray("meals").getJSONObject(position).get("idMeal").toString();
                         String title = mRecipes.getJSONArray("meals").getJSONObject(position).get("strMeal").toString();
                         String instructions = mRecipes.getJSONArray("meals").getJSONObject(position).get("strInstructions").toString();
                         ArrayList<String> ingredients = new ArrayList<>();
+
+                        Boolean isFavourite = favouriteRecipes.contains(recipeId);
+                        Log.d("ASDLKFJASLKDFSLAKJDF", isFavourite.toString());
                         for(int i = 1; i <= getNumIngredients(position); i++) {
                             ingredients.add(mRecipes.getJSONArray("meals").getJSONObject(position).get("strIngredient"+i).toString());
                         }
-                        RecipeDetails recipeDetails = RecipeDetails.newInstance(URL, title, ingredients, instructions);
+                        RecipeDetails recipeDetails = RecipeDetails.newInstance(recipeId, URL, title, ingredients, instructions, isFavourite);
                         recipeDetails.show(fragm, null);
                     }catch (JSONException e){
 
@@ -130,19 +165,24 @@ public class RecipeSearchAdapter extends RecyclerView.Adapter<RecipeSearchAdapte
         Picasso.with(context).load(url).into(imageView);
     }
     public static class RecipeDetails extends DialogFragment {
+        String recipeId;
         String url;
         String title;
         String instructions;
         ArrayList<String> ingredients;
-        public static RecipeDetails newInstance(String imageUrl, String title, ArrayList<String> ingredients, String instructions){
+        Boolean isFavourite = false;
+        public static RecipeDetails newInstance(String recipeId, String imageUrl, String title, ArrayList<String> ingredients, String instructions, Boolean isFavourite){
             RecipeDetails fragment = new RecipeDetails();
+            fragment.recipeId = recipeId;
             fragment.url=imageUrl;
             fragment.title = title;
             fragment.instructions=instructions;
             fragment.ingredients=ingredients;
+            fragment.isFavourite = isFavourite;
             for(String s : ingredients) {
                 Log.d("INGREDIENT", s);
             }
+            Log.d("FIANSDFASNDFLA", isFavourite.toString());
             return fragment;
         }
         @Override
@@ -151,6 +191,7 @@ public class RecipeSearchAdapter extends RecyclerView.Adapter<RecipeSearchAdapte
             // Get the layout inflater
             LayoutInflater inflater = getActivity().getLayoutInflater();
             View view = inflater.inflate(R.layout.recipe_details, null);
+
             Button closeDetails = view.findViewById(R.id.close_details);
             closeDetails.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -158,12 +199,16 @@ public class RecipeSearchAdapter extends RecyclerView.Adapter<RecipeSearchAdapte
                     getDialog().cancel();
                 }
             });
+
             TextView recipeTitle = view.findViewById(R.id.recipe_title);
             recipeTitle.setText(title);
+
             TextView recipeInstructions = view.findViewById(R.id.recipe_instructions);
             recipeInstructions.setText(instructions);
+
             ImageView imageView = view.findViewById(R.id.recipe_image);
             Picasso.with(context).load(url).into(imageView);
+
             TextView recipeIngredients = view.findViewById(R.id.recipe_ingredients);
             StringBuilder sb = new StringBuilder();
             for(String s: ingredients) {
@@ -178,6 +223,7 @@ public class RecipeSearchAdapter extends RecyclerView.Adapter<RecipeSearchAdapte
             final DatabaseReference db;
             db = FirebaseDatabase.getInstance().getReference();
             final DatabaseReference groceryListReference = db.child("list");
+            final DatabaseReference favouriteRecipesReference = db.child("recipes");
 
             view.findViewById(R.id.add).setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -189,6 +235,26 @@ public class RecipeSearchAdapter extends RecyclerView.Adapter<RecipeSearchAdapte
                     Toast.makeText(getContext(), "Added!", Toast.LENGTH_SHORT).show();
                 }
             });
+
+            final ImageButton favourite = view.findViewById(R.id.favourite);
+            favourite.setImageResource(isFavourite ? R.drawable.favourite_full : R.drawable.favourite_empty);
+            favourite.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if(isFavourite) {
+                        favouriteRecipesReference.child(recipeId).removeValue();
+                    } else {
+                        DatabaseReference thisRecipe = favouriteRecipesReference.child(recipeId);
+                        thisRecipe.child("title").setValue(title);
+                        thisRecipe.child("ingredients").setValue(ingredients);
+                        thisRecipe.child("instructions").setValue(instructions);
+                        thisRecipe.child("imageURL").setValue(url);
+                    }
+                    isFavourite = !isFavourite;
+                    favourite.setImageResource(isFavourite ? R.drawable.favourite_full : R.drawable.favourite_empty);
+                }
+            });
+
             builder.setView(view);
             return builder.create();
         }
